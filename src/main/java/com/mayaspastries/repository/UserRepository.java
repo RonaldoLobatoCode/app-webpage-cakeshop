20700285b90b1d5652c78557b5deb45d296339b6
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-01-13 17:17:30 -0500

    Finalizado el desarrolo del repositorio Usuario

--- src/main/java/com/mayaspastries/repository/UserRepository.java
+++ src/main/java/com/mayaspastries/repository/UserRepository.java
@@ -1,5 +1,15 @@
 package com.mayaspastries.repository;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import com.mayaspastries.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Integer>{
+
+	User findByUser(String user);
+	
+	@Query("SELECT u FROM User u WHERE u.user = :user AND u.password = :hashedPassword")
+    User findByUsernameAndHashedPassword(@Param("user") String user, @Param("hashedPassword") String hashedPassword);
 }

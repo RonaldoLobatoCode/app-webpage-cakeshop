37c4d324428ce2f849db01e0fa772a4530823ad3
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-01-13 17:13:22 -0500

    Finalizado el desarrollo de la clase Usuario

--- src/main/java/com/mayaspastries/controller/HomeController.java
+++ src/main/java/com/mayaspastries/controller/HomeController.java
@@ -1,5 +1,14 @@
 package com.mayaspastries.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
 
+	@GetMapping("/home")
+	public String openHome(Model model) {
+		return "index";
+	}
 }
--- src/main/java/com/mayaspastries/entities/User.java
+++ src/main/java/com/mayaspastries/entities/User.java
@@ -1,5 +1,23 @@
 package com.mayaspastries.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "user")
+@Data
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public int iduser;
+	public String name;
+	public String lastname;
+	public String email;
+	public String user;
+	public String password;
 }

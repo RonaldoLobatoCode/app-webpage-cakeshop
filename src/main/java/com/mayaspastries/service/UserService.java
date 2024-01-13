4b8e8345784b4fdb277dcd1b4746fd65f087f345
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-01-13 17:19:52 -0500

    Finalizado el desarrollado del servicio Usuario

--- src/main/java/com/mayaspastries/service/UserService.java
+++ src/main/java/com/mayaspastries/service/UserService.java
@@ -1,5 +1,10 @@
 package com.mayaspastries.service;
 
+import com.mayaspastries.entities.User;
+
 public interface UserService {
 
+	public User getUserAndPassword(String user, String password);
+	
+	User getUser(String username, String providedPassword);
 }

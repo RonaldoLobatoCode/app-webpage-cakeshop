f15ab251119ba55b89d5b09e1f1030fffa7605d8
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-01-14 12:17:42 -0500

    Finalizado el desarrollo del repositorio Producto

--- src/main/java/com/mayaspastries/repository/ProductRepository.java
+++ src/main/java/com/mayaspastries/repository/ProductRepository.java
@@ -1,5 +1,16 @@
 package com.mayaspastries.repository;
 
-public interface ProductRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.mayaspastries.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Integer>{
+
+	@Query("SELECT p FROM Product p WHERE p.name LIKE %?1%")
+	public List<Product> findAll(String searchWord);
+	
+	List<Product> findByidcategory(Integer idcategory);
 }

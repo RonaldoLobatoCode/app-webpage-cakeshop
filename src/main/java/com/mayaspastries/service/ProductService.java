a6de0c50df31ae53926e5c47b828253a06d2b63b
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-01-14 12:24:14 -0500

    Finalizado el desarrollo del servicio Producto

--- src/main/java/com/mayaspastries/service/ProductService.java
+++ src/main/java/com/mayaspastries/service/ProductService.java
@@ -1,5 +1,25 @@
 package com.mayaspastries.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.mayaspastries.entities.Product;
+
 public interface ProductService {
 
+	public List<Product> listProduct();
+	
+	public Product addProduct(Product objProduct);
+	
+	public Product getProductById(int productId);
+	
+	public List<Product> findProductByName(String searchWord);
+	
+	public Optional<Product> getProductForId(Integer productId);
+	
+	public Product updateProduct(Product objProduct);
+	
+	public void deleteProduct(int idproduct);
+	
+	public List<Product> getRandomProducts(int numberOfProducts);
 }

f59303147daf904aadc81872056e9e1e0554a187
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-01-14 12:28:56 -0500

    Implementacion de la interfaz ProductService en la clase ProductImpl

--- src/main/java/com/mayaspastries/implement/ProductImpl.java
+++ src/main/java/com/mayaspastries/implement/ProductImpl.java
@@ -1,5 +1,59 @@
 package com.mayaspastries.implement;
 
-public class ProductImpl {
+import java.util.List;
+import java.util.Optional;
+
+import com.mayaspastries.entities.Product;
+import com.mayaspastries.service.ProductService;
+
+public class ProductImpl implements ProductService{
+
+	@Override
+	public List<Product> listProduct() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Product addProduct(Product objProduct) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Product getProductById(int productId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Product> findProductByName(String searchWord) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Optional<Product> getProductForId(Integer productId) {
+		// TODO Auto-generated method stub
+		return Optional.empty();
+	}
+
+	@Override
+	public Product updateProduct(Product objProduct) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteProduct(int idproduct) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Product> getRandomProducts(int numberOfProducts) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }

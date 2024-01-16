191a5b162e9aec24cc2f0c78f540c9261cece543
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-01-16 17:44:15 -0600

    Finalizado el desarrollo de la clase Categoria

--- src/main/java/com/mayaspastries/entities/Category.java
+++ src/main/java/com/mayaspastries/entities/Category.java
@@ -1,5 +1,19 @@
 package com.mayaspastries.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "category")
+@Data
 public class Category {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idcategory;
+    private String name;
 }

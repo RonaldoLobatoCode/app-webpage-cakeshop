428014f460391b117b0e39832d808e2d19dce6a4
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-01-16 11:41:28 -0500

    Implementacion de la interface IUploadService en la clase IUploadImpl

--- src/main/java/com/mayaspastries/implement/IUploadImpl.java
+++ src/main/java/com/mayaspastries/implement/IUploadImpl.java
@@ -1,5 +1,49 @@
 package com.mayaspastries.implement;
 
-public class IUploadImpl {
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.mayaspastries.service.IUploadFileService;
+
+public class IUploadImpl implements IUploadFileService{
+
+	@Override
+	public Resource load(String filename) throws MalformedURLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String copy(MultipartFile file) throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean delete(String filename) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String getImageNameById(int productId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean deleteImageFromUploads(String imageName) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String saveImageToUploads(MultipartFile imageFile) throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }

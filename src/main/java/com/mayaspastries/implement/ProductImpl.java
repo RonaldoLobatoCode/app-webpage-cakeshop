21d5bee89617a73be14f52156b46a69faad2d729
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-01-16 11:35:48 -0500

    Finalizado el desarrollo de implementacion de la clase ProductImpl

--- src/main/java/com/mayaspastries/implement/ProductImpl.java
+++ src/main/java/com/mayaspastries/implement/ProductImpl.java
@@ -1,59 +1,99 @@
 package com.mayaspastries.implement;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.stereotype.Service;
+
 import com.mayaspastries.entities.Product;
+import com.mayaspastries.repository.ProductRepository;
 import com.mayaspastries.service.ProductService;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
 public class ProductImpl implements ProductService{
 
+	private ProductRepository repoProduct;
+	
+	@PersistenceContext
+	private EntityManager entityManager;
+	
 	@Override
 	public List<Product> listProduct() {
 		// TODO Auto-generated method stub
-		return null;
+		return repoProduct.findAll();
 	}
 
 	@Override
 	public Product addProduct(Product objProduct) {
 		// TODO Auto-generated method stub
-		return null;
+		return repoProduct.save(objProduct);
 	}
 
 	@Override
 	public Product getProductById(int productId) {
 		// TODO Auto-generated method stub
-		return null;
+		return repoProduct.findById(productId).orElse(null);
 	}
 
 	@Override
 	public List<Product> findProductByName(String searchWord) {
 		// TODO Auto-generated method stub
-		return null;
+		return repoProduct.findAll(searchWord);
 	}
 
 	@Override
 	public Optional<Product> getProductForId(Integer productId) {
 		// TODO Auto-generated method stub
-		return Optional.empty();
+		return repoProduct.findById(productId);
 	}
 
 	@Override
 	public Product updateProduct(Product objProduct) {
 		// TODO Auto-generated method stub
-		return null;
+		return repoProduct.save(objProduct);
 	}
 
 	@Override
 	public void deleteProduct(int idproduct) {
 		// TODO Auto-generated method stub
-		
+		repoProduct.deleteById(idproduct);
 	}
-
+	
 	@Override
 	public List<Product> getRandomProducts(int numberOfProducts) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Integer> allProductIds = getAllProductIds();
+		Collections.shuffle(allProductIds);
+
+		List<Product> randomProducts = new ArrayList<>();
+		for (int i = 0; i < numberOfProducts && i < allProductIds.size(); i++) {
+			int productId = allProductIds.get(i);
+			Product product = getProductByIdRamdom(productId);
+			if (product != null) {
+				randomProducts.add(product);
+			}
+		}
+
+		return randomProducts;
 	}
 
+	private List<Integer> getAllProductIds() {
+		// Obtén una lista de todos los IDs de productos disponibles en tu base de
+		// datos.
+		Query query = entityManager.createQuery("SELECT p.id FROM Product p");
+		List<Integer> allProductIds = query.getResultList();
+		return allProductIds;
+	}
+
+	 private Product getProductByIdRamdom(int productId) {
+	        // Obtén un producto por su ID.
+	        return entityManager.find(Product.class, productId);
+	    }
 }

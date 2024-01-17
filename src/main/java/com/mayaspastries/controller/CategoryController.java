7284de7cc6a7f7f8ad6d3baadf887c82a0a6f7cd
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-01-16 19:59:58 -0600

    Añadir endpoint de actualizar informacion de categoria en el metodo updateCategory en el clase controlador CategoryController

--- src/main/java/com/mayaspastries/controller/CategoryController.java
+++ src/main/java/com/mayaspastries/controller/CategoryController.java
@@ -1,5 +1,6 @@
 package com.mayaspastries.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import com.mayaspastries.entities.Category;
 import com.mayaspastries.service.CategoryService;
@@ -55,5 +57,11 @@ public class CategoryController {
     public String addCategory(@ModelAttribute("category") Category category) {
         serviceCategory.addCategory(category);
         return "redirect:/maintenance/category";
+    }
+
+    @PutMapping("/category")
+    public ResponseEntity<String> updateCategory(@ModelAttribute("category") Category category) {
+        serviceCategory.updateCategory(category);
+        return new ResponseEntity<>("success", HttpStatus.OK);
     }
 }

8994294198a20e4d4c5eaf54f1587dab18cc2846
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-01-16 18:05:16 -0600

    Finalizado el desarrollo del repositorio Categoria

--- src/main/java/com/mayaspastries/repository/CategoryRepository.java
+++ src/main/java/com/mayaspastries/repository/CategoryRepository.java
@@ -1,5 +1,15 @@
 package com.mayaspastries.repository;
 
-public interface CategoryRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.mayaspastries.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
+    @Query("SELECT c FROM Category c WHERE c.name LIKE %?1%")
+    public List<Category> findAll(String searchWord);;
 }
+        

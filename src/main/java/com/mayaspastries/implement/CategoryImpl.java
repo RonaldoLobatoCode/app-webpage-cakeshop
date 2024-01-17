6ae70564687bcd2826bc7b58f090fb7291b143e0
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-01-16 19:26:01 -0600

    Finalizado el desarrollo del servicio de la clase CategoryService

--- src/main/java/com/mayaspastries/implement/CategoryImpl.java
+++ src/main/java/com/mayaspastries/implement/CategoryImpl.java
@@ -1,5 +1,5 @@
 package com.mayaspastries.implement;
 
-public class CategoryImpl {
+public class CategoryImpl{
 
 }
--- src/main/java/com/mayaspastries/service/CategoryService.java
+++ src/main/java/com/mayaspastries/service/CategoryService.java
@@ -1,5 +1,22 @@
 package com.mayaspastries.service;
 
+import java.util.List;
+
+import com.mayaspastries.entities.Category;
+
 public interface CategoryService {
 
+    public List<Category> listCategory();
+
+    public Category addCategory(Category objCategory);
+
+    public Category getCategoryById(int idcategory);
+
+    public Category updateCategory(Category objCategory);
+
+    public List<Category> findCategoryByName(String searchWord);
+
+    public boolean hasProduct(Integer idcategory);
+
+    public void deleteCategort(int idcategory);
 }

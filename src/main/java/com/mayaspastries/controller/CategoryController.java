c039ea1d993d2e2c7152500fabe9180c644812d5
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-01-16 19:50:00 -0600

    Añadir lógica de acceso a la página de mantenimiento de categorías en el controlador Categoria

--- src/main/java/com/mayaspastries/controller/CategoryController.java
+++ src/main/java/com/mayaspastries/controller/CategoryController.java
@@ -1,5 +1,42 @@
 package com.mayaspastries.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.mayaspastries.entities.Category;
+import com.mayaspastries.service.CategoryService;
+import com.mayaspastries.service.EmployeeService;
+
+import jakarta.servlet.http.HttpSession;
+import lombok.AllArgsConstructor;
+
+@Controller
+@AllArgsConstructor
 public class CategoryController {
 
+    private CategoryService serviceCategory;
+
+    private EmployeeService serviceEmployee;
+
+    @GetMapping("/maintenance/category")
+    public String openPageCategory(Model model, HttpSession session) {
+
+        Integer currentUserId = (Integer) session.getAttribute("userId");
+
+        if (currentUserId != null) {
+            int userId = currentUserId;
+
+            Integer employeeId = serviceEmployee.getEmployeeIdByUsername(userId);
+
+            if (employeeId != null) {
+                model.addAttribute("listCategory", serviceCategory.listCategory());
+                model.addAttribute("category", new Category());
+                model.addAttribute("employeeId", employeeId);
+                return "maintenance-category";
+
+            }
+        }
+        return "redirect:/login";
+    }
 }

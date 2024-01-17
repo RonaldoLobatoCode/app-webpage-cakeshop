e119190c107b005d184b72e1526601180f5d3624
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-01-16 19:43:50 -0600

    Añadir carga de productos aleatorios en la pagina de inicio en el la clase HomeController

--- src/main/java/com/mayaspastries/controller/HomeController.java
+++ src/main/java/com/mayaspastries/controller/HomeController.java
@@ -1,17 +1,30 @@
 package com.mayaspastries.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.mayaspastries.entities.Product;
 import com.mayaspastries.entities.User;
+import com.mayaspastries.service.ProductService;
+
+import lombok.AllArgsConstructor;
 
 @Controller
+@AllArgsConstructor
 public class HomeController {
+
+	private ProductService serviceProduct;
 
 	@GetMapping("/home")
 	public String openHome(Model model) {
+		List<Product> randomProducts = serviceProduct.getRandomProducts(5);
+
+		model.addAttribute("randomProducts", randomProducts);
+
 		return "index";
 	}
 

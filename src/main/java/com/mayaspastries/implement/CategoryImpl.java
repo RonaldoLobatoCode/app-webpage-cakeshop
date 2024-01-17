3d14678ee4aca520087f5b199b041847eac2d896
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-01-16 19:29:25 -0600

    Implementacion de la interface CategoryService en la clase CategoryImpl

--- src/main/java/com/mayaspastries/implement/CategoryImpl.java
+++ src/main/java/com/mayaspastries/implement/CategoryImpl.java
@@ -1,5 +1,52 @@
 package com.mayaspastries.implement;
 
-public class CategoryImpl{
+import java.util.List;
+
+import com.mayaspastries.entities.Category;
+import com.mayaspastries.service.CategoryService;
+
+public class CategoryImpl implements CategoryService {
+
+    @Override
+    public List<Category> listCategory() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'listCategory'");
+    }
+
+    @Override
+    public Category addCategory(Category objCategory) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addCategory'");
+    }
+
+    @Override
+    public Category getCategoryById(int idcategory) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getCategoryById'");
+    }
+
+    @Override
+    public Category updateCategory(Category objCategory) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateCategory'");
+    }
+
+    @Override
+    public List<Category> findCategoryByName(String searchWord) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findCategoryByName'");
+    }
+
+    @Override
+    public boolean hasProduct(Integer idcategory) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'hasProduct'");
+    }
+
+    @Override
+    public void deleteCategort(int idcategory) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteCategort'");
+    }
 
 }

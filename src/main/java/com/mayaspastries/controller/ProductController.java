59da1dfabed698104d3fdb4818a0de84cadeeef7
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-01-18 15:08:33 -0600

    Añadir controlador de productos con servicios relacionados

--- src/main/java/com/mayaspastries/controller/ProductController.java
+++ src/main/java/com/mayaspastries/controller/ProductController.java
@@ -1,5 +1,22 @@
 package com.mayaspastries.controller;
 
+import org.springframework.stereotype.Controller;
+
+import com.mayaspastries.service.CategoryService;
+import com.mayaspastries.service.EmployeeService;
+import com.mayaspastries.service.IUploadFileService;
+import com.mayaspastries.service.ProductService;
+
+import lombok.AllArgsConstructor;
+
+@Controller
+@AllArgsConstructor
 public class ProductController {
+
+    private CategoryService serviceCategory;
+    private EmployeeService serviceEmployee;
+    private ProductService serviceProduct;
+    private IUploadFileService uploadFileService;
+    
 
 }
